998b62aebadc5ab6252711f48995021ab62df0aa
ito <ito@NCCT20250213.local>
2025-09-17 16:34:23 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,36 @@
-//! ライブラリクレートのドキュメント
+//! これはライブラリクレートのドキュメント
 
 /// say_hello関数のドキュメント
 pub fn say_hello() {
     println!("Hello!");
 }
 
+//トレイトにはメソッドのシグネチャーのみを実装する．トレイトに記載されたメソッドの実装を強制する
+//任意の型とトレイトを結びつけることをトレイトを実装するとよぶ
+//main.rsに定義することもできるが今回はlib.rsに入れる
+//// 構造体のメソッドの定義はこれだった
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }//selfの型名は省略可
+
+//     //型関連関数．この場合は&selfは第1引数にならない
+//     //明示的にnew関数を置くことにより，インスタンス生成の歳に特定の条件を満たしているかを検証するロジックを組み込める
+//     // 特定のクラスのインスタンスが生成されるときに自動的に呼び出されるメソッドをコンストラクタと呼ぶが
+//     // Rustではコンストラクタという概念はないので，明示的にnew()で型関連関数を呼ぶ
+//     fn new(width: u32, height: u32) -> Self{
+//         Rectangle {width, height}
+//     }
+// }
+
 pub mod sample_trait {
     pub trait Shape {
         fn calc_area(&self) -> f64;
         fn calc_perimeter(&self) -> f64;
-        fn do_something();
+        fn do_something(); //構造体と同様に，引数にselfを取らない型関連関数も定義できる
+        fn default_something(&self) -> &str {
+            "This is default method!" //デフォルトメソッド
+        }
     }
 
     pub struct Rectangle {
@@ -27,6 +48,9 @@ pub mod sample_trait {
 
         fn do_something() {
             println!("This is Rectangle function");
+        }
+        fn default_something(&self) -> &str {
+            "This is rectangle default!"
         }
 
 
@@ -48,5 +72,12 @@ pub mod sample_trait {
             println!("This is circle function");
         }
     }
+
+      //面積の2倍という関数を実装するときに，トレイトを共有するすべての構造体に対する参照を渡して定義する
+      //これは関数なので，呼び出すときはインスタンスに対する参照（&）を渡す
+    pub fn double_area(shape: &impl Shape) -> f64{
+        shape.calc_area() * 2.0
+    }
+
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Debug,Display};
 // mod basics;
 // mod basics2;
 // mod basics3;
@@ -5,8 +6,85 @@
 //mod basics5;
 mod basics6;
 //mod basics6::submodules1;
-use rust_tutorial::sample_trait::{Shape,Rectangle, Circle};
+use rust_tutorial::sample_trait::{Shape, Rectangle, Circle, double_area};
 
+
+//引数の型を固定しないように関数を作ることで，関数の重複を防げる．例えば以下の関数は，aやbに整数以外が入るともう機能しない
+// fn max(a: i32, b: i32) -> i32 {
+//     if a >= b {
+//         a
+//     } else {
+//         b
+//     }
+// }
+
+//ジェネリック型を用いた関数について
+fn max<T>(a: T, b: T) -> T //max<T:PartialOrg> でも可
+    where T: PartialOrd + Debug
+{
+    if a >= b {
+        a
+    } else {
+        b
+    }
+}
+
+//ジェネリック型を用いた構造体について
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T:PartialOrd + Debug> Point<T> {
+    fn max(&self) ->&T {
+        if self.x >= self.y {
+            &self.x
+        } else {
+            &self.y
+        }
+    }
+
+    //メソッド定義のジェネリック型と構造体のジェネリック型は必ずしも一致している必要はない
+    //ここで，他のトレイト境界を持つような変数を用いた関数を定義してみる
+    fn print_arg<U: Display>(&self, val: U) {
+        println!("self.x:{:?}", self.x);
+        println!("val: {}", val);
+
+    }
+}
+
+//特定の型の値の場合のみ機能するメソッド
+impl Point<i32> {
+    fn min(&self) -> i32 {
+        if self.x <= self.y {
+            self.x
+        } else {
+            self.y
+        }
+    }
+}
+
+//エラーハンドリングはResult型で行う
+//Rustでは他の言語にあるようなエラーの爪がない
+fn need_even(a: i32) -> Result<i32, String> {
+    if a % 2 == 0 {
+        Ok(a)
+    } else {
+        Err(String::from("引数は偶数にしてください．"))
+    }
+}
+
+//エラーの移譲について
+
+fn double_even(b: i32) -> Result<i32, String> {
+    // match need_even(b) {
+    //     Ok(val) => Ok(val * 2),
+    //     Err(err) => Err(err),
+    // }
+    let x = need_even(b)?; //もし戻り値がOk(val)であればOkの中身であるvalを取り出してxに詰める，戻り値がErrであればそれをすぐに関数double_evenの呼び出し元に返す
+    Ok(x * 2)
+}
+//これは，エラーハンドリングを関数側で規定せず，main()にその処理を移譲していることになる
 
 fn main() {
     //basics::run();
@@ -35,7 +113,138 @@ fn main() {
     let test_structure = submodule1::teststruct::new(1, 2);
     rust_tutorial::say_hello();
 
-    let rect = Rectanle{}
+    let rect = Rectangle{width: 2.0, height: 3.0};
+    println!("{} and {}", rect.width, rect.height);
+    let circ = Circle {
+        radius: 2.0
+    };
+    Circle::do_something(); //do_somethingはprintln!を返す．関数
+    println!("{}", circ.default_something()); //circ.default_somethingは単に&strを
+
+    println!("Rectangle double area: {}", double_area(&rect));
+
+    println!("{:?}", (1, 2, 3));
+
+    #[derive(Debug, PartialEq)] //構造体を作るときは，いろいろと制約がある．deriveでDebugを乗せる必要がある
+    // 自分で定義した構造体に演算など特定のメソッドを持たせたい場合、それに対応するトレイトを実装する必要があります
+    struct S {
+        val1: i32, 
+        val2: i32,
+    }
+
+    let s_test = S {val1: 2, val2: 5};
+    println!("s_test is test {:?}", s_test);
+
+    println!("{:?}", S {val1: 1, val2: 2});
+
+    let s1 = S {
+        val1: 1,
+        val2: 2
+    };
+
+    let s2 = S {
+        val1: 1,
+        val2: 3,
+    };
+
+    println!("{}", s1 == s2);
+
+    println!("{}", max(2.0, 4 as f64));
+
+
+    let p1 = Point {x: 2, y: 1};
+    let p2 = Point {x: 2.0, y: 1.0};
+    let p3 = Point {x: "a", y: "d"};
+    println!("p1.max: {}", p1.max());
+    p1.print_arg("test");
+    p1.min(); //これはエラーにならない
+    //p2.min() //これはエラーになる
+
+    //パニックが起こると一時的に巻き戻しが起こり，順番に破棄されていく
+
+    println!("{}", [1,2,3][0]);
+    // 自力でパニックを発生させることもできる
+    // panic!("This is my panic!");
+
+
+    //result型を返す関数を使う場合，matchによる条件分岐が強制される
+    let x = match need_even(2) {
+        Ok(val) => val,
+        Err(err) => {
+            println!("Error message: {}", err);
+            panic!()
+        }
+    };
+    println!("{}", x);
+
+    let s = need_even(1);
+    println!("{}", s.is_ok()); //これはboolを返す
+    println!("{}", s.is_err()); //これは
+
+    // println!("{:?}",s.ok()); //some(2)またはNoneが返される．ただしここでは所有権が移動する！！！！
+
+    // println!("{:?}", s.unwrap_or(0)); //ResultがOkならばその中の値を返し，Errならば引数のデフォルト値を返す
+    // println!("{:?}", s.expect("expectから発生")); //matchを使うよりも簡潔なハンドリングができる
+
+    println!("{:?}", double_even(3));
+
+    match double_even(2) {
+        Ok(val) => println!("{}", val),
+        Err(err) => {
+            println!("mainでハンドリング");
+            println!("{}", err)
+        }
+    }
+
+    let c1 = |x: i32| x + 1;
+    println!("{:?}", c1(10)); //クロージャの引数を渡す
+
+    let m = 10;
+    let c2 =  |x:i32| x + m;
+    println!("{:?}", c2(10));
+    println!("{:?}", m);
+
+    let m = 20;
+    println!("{:?}", c2(20)); //クロージャ生成時点でのmの値を使い続ける，値の更新はない
+
+
+    let v = vec![1, 2, 3];
+    let c3 =  || { //ここでmoveを打つと，所有権の移動が起こるので，最終行がエラーになる
+        println!("{:?}", v)
+    };
+    c3();
+     println!("{:?}", v); //そのままだと変数の所有権の移動は起こらない
+
+
+     //イテレータの例
+     let v = vec![1, 2, 3, 4, 5];
+     let v1_iter = v.iter();
+     for x in v1_iter {
+        println!("{:?}", x);
+     }
+
+     //next()を使うと，イテレータの消費が起こる
+     //next()メソッドが呼ばれるたびに、イテレータの状態（次にどの要素を返すかの情報）が更新されるため，mutが必要
+     let mut v2_iter = v.iter();
+     println!("{:?}", v2_iter.next());
+     println!("{:?}", v2_iter.next());
+     println!("{:?}", v2_iter.next());
+     println!("{:?}", v2_iter.next());
+     println!("{:?}", v2_iter.next());
+     println!("{:?}", v2_iter.next());
+
+ 
+
+
+
+
+
+
+
+
+    
+
+
 
 
 
